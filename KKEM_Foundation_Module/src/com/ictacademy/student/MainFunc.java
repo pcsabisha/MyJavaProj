dac321262bae454917ce0575e44d4bfc0b5814a2
sabishapc <95956824+sabishapc@users.noreply.github.com>
2022-08-05 17:58:26 +0530

    Update MainFunc.java

--- KKEM_Foundation_Module/src/com/ictacademy/student/MainFunc.java
+++ KKEM_Foundation_Module/src/com/ictacademy/student/MainFunc.java
@@ -5,7 +5,7 @@ public class MainFunc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		myfunction obj=new myfunction();
-	System.out.println(	obj.great(6, 5));
+	System.out.println(	obj.great(9, 5));
 
 	}
 
